d0c2251b5f0241adc6e3f5c4df4d12839971ea12
debopamgupta <debopamgupta3000@gmail.com>
2020-04-29 17:28:23 +0530

    Adding node-fetch and api calling and wrapping up

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 require("dotenv").config();
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 client.once("ready", () => {
-  console.log("BEEP BOOP ! Ready! 🤖 ");
+  console.log("BEEP BOOP ! Ready!");
 });
 
 client.on("message", async (msg) => {
-  // for edutips unlimited bot spam channel
-
+  // for bot spam channel only and danky memes
   if (
-    msg.channel.id === "704635164386525235" &&
-    msg.guild.id === "613691957142880256"
+    (msg.channel.id === "559781407753240588" &&
+      msg.guild.id === "442723788732497936") ||
+    (msg.channel.id === "704635164386525235" &&
+      msg.guild.id === "613691957142880256")
   ) {
-    if (msg.content.toLowerCase() === "ping") {
-      await msg.channel.send("pong ! ");
-      console.log("reply sent");
-    }
-
     let regex = /^!meme/i;
 
     if (regex.exec(msg.content)) {
-      await msg.channel.send("memes coming peeps....");
+      const response = await fetch("https://www.reddit.com/r/memes/.json");
+      const json = await response.json();
+      const memeIndex = getRandomInt(1, json.data.dist);
+      const memeUrl = json.data.children[memeIndex].data.url;
+      const memeTitle = json.data.children[memeIndex].data.title;
+      await msg.channel.send(memeTitle);
+      await msg.channel.send(memeUrl);
     }
   }
 });
